798862eecaae5c612ba41358bfd315acb2dc55a3
copierrj <reijer.copier@idgis.nl>
2016-03-09 14:15:29 +0100

    docker-compose upgraded

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -13,7 +13,7 @@ apt-get -qy upgrade
 
 if [[ ! -e /usr/local/bin/docker-compose ]]; then
 
-	curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 	
 fi
