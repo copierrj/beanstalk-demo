1fc9bf1d8fe2f13982280ba2b9f4123a93d78575
copierrj <reijer.copier@idgis.nl>
2016-03-09 12:38:24 +0100

    consumer and producer implementation added

--- consumer/src/main/java/Consumer.java
+++ consumer/src/main/java/Consumer.java
@@ -1,6 +1,22 @@
+import com.surftools.BeanstalkClient.Client;
+import com.surftools.BeanstalkClient.Job;
+import com.surftools.BeanstalkClientImpl.ClientImpl;
+
 public class Consumer {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Consumer started");
+		
+		Client client = new ClientImpl();
+		client.useTube("demo");
+		
+		for(;;) {
+			Job job = client.reserve(null);
+			
+			String message = new String(job.getData(), "utf-8");
+			System.out.println("message received: '" + message + "'");
+			
+			client.delete(job.getJobId());
+		}
 	}
 }
--- producer/src/main/java/Producer.java
+++ producer/src/main/java/Producer.java
@@ -1,6 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import com.surftools.BeanstalkClient.Client;
+import com.surftools.BeanstalkClientImpl.ClientImpl;
+
 public class Producer {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Producer started");
+		
+		Client client = new ClientImpl();
+		client.useTube("demo");
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			for(;;) {
+				System.out.print("> ");
+				System.out.flush();
+				
+				String command = br.readLine().trim();
+				
+				if(command.equalsIgnoreCase("exit")) {
+					break;
+				}
+				
+				if(command.toLowerCase().startsWith("put")) {
+					String message = command.substring(4).trim();
+					
+					client.put(0, 0, 1, message.getBytes("utf-8"));
+					
+					System.out.println("put message: '" + message + "'");
+					continue;
+				}
+				
+				System.out.println("unknown command: '" + command + "'");
+			}
+		}
+		
+		System.out.println("Producer terminated");
 	}
 }
